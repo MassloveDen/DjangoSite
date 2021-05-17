4cd5f2f7c1e3e7ad929de26c71e6e0a4095ccac0
MassloveDen <denis132141@yandex.ru>
2021-05-17 17:34:19 +0300

    update

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
         return render(
-            request,
-            'myblog/home.html'
-        )
+            request, 'myblog/home.html',
+            context= {'posts': posts
+        })
 # Create your views here.
