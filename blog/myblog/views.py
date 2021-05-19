1fdfa7d8335dbcb0ec25ec753aa97c0f48d9a201
MassloveDen <denis132141@yandex.ru>
2021-05-19 08:14:06 +0300

    update

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+
 from .models import Post
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         return render(
             request, 'myblog/home.html',
-            context= {'posts': posts
+            context= {'page_obj': page_obj
         })
 # Create your views here.
